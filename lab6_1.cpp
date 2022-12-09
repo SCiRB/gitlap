0f48962b7d55477eeb47cdd47c697d0f5a1493a3
Lica <Tanapat_choeichomsri@cmu.ac.th>
2022-12-09 16:34:16 +0700

    lap6_1 completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
+    int i = 0;
+    int number[1000];
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> number[0];
+    while (number[i]!=0){
+    i++;
+    cout << "Enter an integer: ";
+    cin >> number[i];
+    }
+    int NE = 0;
+    int NO = 0;
+    int n = 0;
+    while (n<i){
+        if(number[n]!=0){
+            if (number[n]%2==0){
+                NE = NE+1;
+            }
+            else{
+                NO = NO+1;
+            }
+        }
+    n++;
+    }
+    cout << "#Even numbers = " << NE << "\n";
+    cout << "#Odd numbers = " << NO;
     return 0;
 }
