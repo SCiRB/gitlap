a9f632fab684697cdaf814770db299fd4420e544
Lica <Tanapat_choeichomsri@cmu.ac.th>
2022-12-09 16:35:16 +0700

    lap6_1 completed

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i = 0;
+    int number[10];
+    cout << "Enter an integer: ";
+    cin >> number[0];
+    while (number[i]!=0){
+    i++;
+    cout << "Enter an integer: ";
+    cin >> number[i];
+    }
+    int NE = 0;
+    int NO = 0;
+    int n = 0;
+    while (n<i){
+        cout << number[n];
+        if(number[n]!=0){
+            if (number[n]%2==0){
+                NE = NE+1;
+            }
+            else{
+                NO = NO+1;
+            }
+        }
+    n++;
+    }
+    cout << "#Even numbers = " << NE << "\n";
+    cout << "#Odd numbers = " << NO;
+    return 0;
+}
