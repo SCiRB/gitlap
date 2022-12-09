24100da61eec6cc602e61a8247755145461b7f9e
Lica <Tanapat_choeichomsri@cmu.ac.th>
2022-12-09 17:11:23 +0700

    lap6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+double deg2rad(double a){
+    a = (a*(M_PI))/180;
+    return a;
+}
+
+double rad2deg(double b){
+    b = (b*180)/(M_PI);
+    return b;
+}
+
+double findXComponent(double l1, double l2, double a1, double a2){
+    return (l1*cos(a1))+(l2*cos(a2));
+}
+
+double findYComponent(double l1, double l2, double a1, double a2){
+    return (l1*sin(a1))+(l2*sin(a2));
+}
+
+double pythagoras(double x, double y){
+    return sqrt(pow(x,2)+pow(y,2));
+}
+
+void showResult(double x,double y){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = " << x << "\n";
+    cout << "Direction of the resultant vector (deg) = " << y << "\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
